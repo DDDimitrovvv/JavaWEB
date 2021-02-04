7925342239ec079adbf267ae2407f778a2cc56bd
DDDimitrovvv <cafuneck@gmail.com>
2021-02-04 08:27:12 +0200

    Project mobilele: Updated Login functionality.

---
+++ mobilele/src/main/java/bg/softuni/mobilele/service/UserService.java
@@ -0,0 +1,10 @@
+package bg.softuni.mobilele.service;
+
+public interface UserService {
+
+    //returns true if the user is authenticated successfully
+    boolean authenticate(String username, String password);
+
+    void loginUser(String username);
+
+}
